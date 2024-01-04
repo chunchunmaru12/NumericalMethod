b310bf48d01633a85fa74a4af8621f546b634c2c
chunchunmaru12 <princekhan456.pk10@gmail.com>
2024-01-04 19:25:30 +0545

    derivate

--- derivatives.c
+++ derivatives.c
@@ -1,21 +1,16 @@
 #include <stdio.h>
 #include <math.h>
-
-// Define the function for which you want to calculate the derivative
 double myFunction(double x) {
-    return x * x; // Example function x^2
+    return x * x;
 }
 
-// Numerical differentiation function
 double calculateDerivative(double (*function)(double), double x, double h) {
     return (function(x + h) - function(x)) / h;
 }
 
 int main() {
-    double x; // Point at which to calculate the derivative
-    double h = 0.0001; // Step size for numerical differentiation
-
-    // Calculate the derivative
+    double x;
+    double h = 0.0001;
     double derivative; 
     for(x=0.0;x<10.0;x++){
         derivative=calculateDerivative(myFunction, x, h);
